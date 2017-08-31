eddc36365bdb563e442781f28b3ce1b103e19565
AlexFolland <lexlexlex@gmail.com>
2017-08-31 11:29:40 -0700

    trim source string better

--- PlaySoundFixer.lua
+++ PlaySoundFixer.lua
@@ -14,7 +14,9 @@ PlaySound = function(sound, ...)
         converted = converted .. c:upper()
     end
 
-    local source = gsub(strtrim(debugstack(2,1,0),".\n"),"Interface\\AddOns\\","")
+    local patterns = {"\n", "^.-AddOns%\\", ": in function.*$"}
+    local source = debugstack(2,1,0)
+    for i = 1, #patterns do source = gsub(source, patterns[i], "") end
     print("PSF: broken PlaySound(\""..soundstring.."\") called from "..source..(SOUNDKIT[converted] and " and converted to ".."PlaySound(SOUNDKIT["..converted.."]) for playback" or ""))
 
     if SOUNDKIT[converted] then return oldPlaySound(SOUNDKIT[converted], ...) end
